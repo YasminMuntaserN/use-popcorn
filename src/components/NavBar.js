95ced1825548b3e2afa43238e88902a6c12ea6e7
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-10 14:28:33 +0300

    import Search Bar to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,13 @@
-import { useState } from "react";
+import { Search } from "./Search";
 
 export function NavBar({movies}){
-  const [query, setQuery] = useState("");
   return (
 <nav className="nav-bar">
         <div className="logo">
           <span role="img">🍿</span>
           <h1>usePopcorn</h1>
         </div>
-
+        <Search/>
         <p className="num-results">
           Found <strong>{movies.length}</strong> results
         </p>
