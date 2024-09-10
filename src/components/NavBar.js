54b0668719e9f9d26c8bd5bb8d7d04153d278de0
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-10 14:24:22 +0300

    feat:add Search Comonent

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,13 +8,7 @@ export function NavBar({movies}){
           <span role="img">🍿</span>
           <h1>usePopcorn</h1>
         </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+
         <p className="num-results">
           Found <strong>{movies.length}</strong> results
         </p>
