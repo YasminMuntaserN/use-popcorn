941ea52d656013c5c37bddfe6e20096e0801b289
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-10 14:30:31 +0300

    feat: add Logo as component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,11 @@
 import { Search } from "./Search";
+import { Logo } from "./Logo";
+
 
 export function NavBar({movies}){
   return (
 <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
+        <Logo/>
         <Search/>
         <p className="num-results">
           Found <strong>{movies.length}</strong> results
