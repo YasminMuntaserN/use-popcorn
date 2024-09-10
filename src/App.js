bf25fc0531df51a3610e4a252533d0300f676d4f
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-10 14:07:11 +0300

    feat: add NavBar component

--- src/App.js
+++ src/App.js
@@ -63,22 +63,7 @@ export default function App() {
 
   return (
     <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
-      </nav>
+      
 
       <main className="main">
         <div className="box">
